3fa3e06c281e500ff2560c0d783270f3b2291bd1
Dustin Jones <coding.dustin@gmail.com>
2022-06-08 09:15:18 -0500

    Fundamentals 1 in progress

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,16 @@
+// Lecture 1 - Variables Assignment
+let country = "United States";
+let continent = "North America";
+let population = 329000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
